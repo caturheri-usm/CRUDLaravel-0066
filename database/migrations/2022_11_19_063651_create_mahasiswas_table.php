6123e42c6ca7bec315908cf8d19f34c7374b5c19
Catur Heri Prayitno <caturheriprayitno@gmail.com>
2022-11-21 19:18:26 +0700

    Kommit Pertama

---
+++ database/migrations/2022_11_19_063651_create_mahasiswas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->char('nim',15)->primary();
+            $table->char('nama',150);
+            $table->integer('umur');
+            $table->char('alamat',255);
+            $table->char('kota',100);
+            $table->char('kelas',50);
+            $table->char('jurusan',50);
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
